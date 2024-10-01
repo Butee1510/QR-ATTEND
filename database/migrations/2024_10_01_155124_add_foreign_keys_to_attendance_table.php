7f0012279e7f7f3c4ae1f2b10bcd69ec9f297b1a
Butee1510 <khaleefasadeeq06@gmail.com>
2024-10-01 18:56:06 +0100

    updated version

---
+++ database/migrations/2024_10_01_155124_add_foreign_keys_to_attendance_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attendance', function (Blueprint $table) {
+            $table->unsignedBigInteger('student_id')->after('id');
+            $table->unsignedBigInteger('course_id')->after('student_id');
+            $table->unsignedBigInteger('lecturer_id')->after('course_id');
+
+
+             // Adding foreign key constraints
+             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+             $table->foreign('lecturer_id')->references('id')->on('lecturers')->onDelete('cascade');
+
+
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attendance', function (Blueprint $table) {
+            $table->dropForeign(['student_id']);
+            $table->dropForeign(['course_id']);
+            $table->dropForeign(['lecturer_id']);
+            $table->dropColumn(['student_id', 'course_id', 'lecturer_id']);
+
+            //
+        });
+    }
+};
